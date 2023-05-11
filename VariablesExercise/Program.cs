ff9895384ecb71759b6818a22cd63b025f8b8fff
Ben Zellers <zellersdenver@gmail.com>
2023-05-11 14:11:08 -0400

    coded message initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Ben";
+            int myWeight = 170;
+            char myFirstInitial = 'B';
+            bool isCoding = true;
+            double myWeights = 170.00;
+            decimal money = 1000.00m;
+            Console.WriteLine($"Hello, my name is {myName}.  I weigh {myWeight}lbs and the first initial of my name is {myFirstInitial}.  It is {isCoding} that I enjoy this class." +
+                $"If I did not weigh {myWeights}, I would most likely have more money than ${money}.");
+
         }
     }
 }
